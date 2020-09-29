7c561b9380ddb0c6149006f18ad265690d08e63d
Georgiy Lebedev <georgiyle@mail.ru>
2020-09-30 00:01:14 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Stack.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/79017/Documents/Work/MIPT/1 semester/Stack/main.cpp" "C:/Users/79017/Documents/Work/MIPT/1 semester/Stack/cmake-build-debug/CMakeFiles/Stack.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
