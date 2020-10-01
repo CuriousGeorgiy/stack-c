8e33bb4abce192f3bfce14c5cc0d430039994ce4
Georgiy Lebedev <georgiyle@mail.ru>
2020-10-01 23:01:03 +0300

    [stack.h]

--- stack.h
+++ stack.h
@@ -1,43 +1,69 @@
 #ifndef STACK_H
 #define STACK_H
 
+/*!
+ * Defines the type of elements stored in the stack
+ */
 typedef double val_t;
 
+/*!
+ * Defines LIFO data structure
+ */
 struct Stack;
 
 /*!
- * @brief
- * @return
+ * Allocates a Stack with non-zero capacity
+ *
+ * @return pointer to a newly allocated Stack
  */
 Stack *stack_construct();
 
 /*!
- * @brief
- * @param stack
- * @param val
- * @return
+ * Pushes val into stack
+ *
+ * @param [in, out] stack pointer to Stack
+ * @param [in] val value to be pushed
+ *
+ * @return 0 in case of success, a non-zero value otherwise
+ *
+ * @note In case of failure doesn't modify stack
  */
 int stack_push(Stack *stack, val_t val);
 
 /*!
- * @brief
- * @param stack
- * @return
+ * Pops a value from stack
+ *
+ * @param [in, out] stack pointer to Stack
+ *
+ * @return the popped value
+ *
+ * @note Returns 0 in case the stack was empty
  */
 val_t stack_pop(Stack *stack);
 
 /*!
- * @brief
- * @param stack
- * @return
+ * Shrinks stack to fit
+ *
+ * @param [in, out] stack pointer to Stack
+ *
+ * @return 0 in case of success, a non-zero value otherwise
  */
-bool stack_ok(Stack *stack);
+int stack_shrink_to_fit(Stack *stack);
 
 /*!
- * @brief
- * @param stack
- * @return
+ * Checks if stack is OK
+ *
+ * @param [in] stack pointer to Stack
+ *
+ * @return true if stack is OK, false otherwise
  */
-void stack_delete(Stack *stack);
+bool stack_ok(const Stack *stack);
+
+/*!
+ * Frees the stack and sets in to a NULL pointer
+ *
+ * @param [in] stack address of pointer to Stack
+ */
+void stack_destruct(Stack **stack);
 
 #endif /* STACK_H */
