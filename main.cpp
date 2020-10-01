c418f452f1f386cbe8c1220938d5e8abf901a87c
Georgiy Lebedev <georgiyle@mail.ru>
2020-10-01 23:00:34 +0300

    [main.cpp]

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-#include <iostream>
+#include <stdio.h>
+
+#include "stack.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Stack *stack = stack_construct();
+
+    for (size_t i = 0; i < 250; ++i) {
+        stack_push(stack, i);
+    }
+
+    for (size_t i = 0; i < 240; ++i) {
+        stack_pop(stack);
+    }
+
+    printf("%d\n", stack_ok(stack));
+
+    stack_shrink_to_fit(stack);
+
+    printf("%d\n", stack_ok(stack));
+
+    stack_destruct(&stack);
+
     return 0;
 }
