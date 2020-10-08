65cdccebe56b9a01955360eb75807b026bea2d05
Georgiy Lebedev <georgiyle@mail.ru>
2020-10-08 21:37:20 +0300

    [main.cpp]

--- main.cpp
+++ main.cpp
@@ -4,23 +4,18 @@
 
 int main()
 {
-    Stack *stack = stack_construct();
+    Stack *stack = new_stack();
 
-    for (size_t i = 0; i < 250; ++i) {
+    for (size_t i = 0; i < 100; ++i) {
         stack_push(stack, i);
     }
 
-    for (size_t i = 0; i < 240; ++i) {
-        stack_pop(stack);
+    for (size_t i = 0; i < 90; ++i) {
+        bool err;
+        stack_pop(stack, &err);
     }
 
-    printf("%d\n", stack_ok(stack));
-
-    stack_shrink_to_fit(stack);
-
-    printf("%d\n", stack_ok(stack));
-
-    stack_destruct(&stack);
+    delete_stack(&stack);
 
     return 0;
 }
