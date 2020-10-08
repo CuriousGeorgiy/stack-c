234842599c1eb7a1d2e575185e9b4651db68f02c
Georgiy Lebedev <georgiyle@mail.ru>
2020-10-08 21:37:08 +0300

    [error.h]

--- error.h
+++ error.h
@@ -5,7 +5,7 @@
 
 #ifdef __clang__
 #define ERROR_OCCURRED_CALLING(func, msg) fprintf(stderr, "ERROR: %s %s at %s(%d):%s\n\n", #func, (msg), __FILE_NAME__, __LINE__, __func__)
-#define ERROR_OCCURRED(msg) fprintf(stderr, "ERROR: %s at %s(%d):%s\n\n", (msg), __FILE_NAME__, __LINE__, __func__)
+#define ERROR_OCCURRED_IN_FUNC(func, msg) fprintf(stderr, "ERROR: in %s: %s at %s(%d):%s\n\n", #func, (msg), __FILE_NAME__, __LINE__, __func__)
 #else
 #define ERROR_OCCURRED_CALLING(func, msg) fprintf(stderr, "%s %s at %s(%d):%s\n\n", #func, (msg), __FILE__, __LINE__, __func__)
 #define ERROR_OCCURRED(msg) fprintf(stderr, "%s at %s(%d):%s\n\n", (msg), __FILE__, __LINE__, __func__)
