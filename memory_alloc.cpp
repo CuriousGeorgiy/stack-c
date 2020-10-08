d25185a251143872591dc09fa26658243afc56c4
Georgiy Lebedev <georgiyle@mail.ru>
2020-10-09 00:34:53 +0300

    [memory_alloc.cpp]

--- memory_alloc.cpp
+++ memory_alloc.cpp
@@ -7,14 +7,14 @@
 
 void *calloc_with_border_canaries(size_t num, size_t size)
 {
-    char *buffer = (char *) calloc(1, CANARY_SIZE + size + CANARY_SIZE);
+    char *buffer = (char *) calloc(1, CANARY_SIZE + num * size + CANARY_SIZE);
 
     if (buffer == NULL) {
         ERROR_OCCURRED_CALLING(calloc, "returned NULL");
         return NULL;
     }
 
-    *((canary_t *) buffer) = *((canary_t *) (buffer + CANARY_SIZE + size)) = CANARY_VALUE;
+    *((canary_t *) buffer) = *((canary_t *) (buffer + CANARY_SIZE + num * size)) = CANARY_VALUE;
 
     return buffer + CANARY_SIZE;
 }
