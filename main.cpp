9fa162e2ecf10b4061563468f3c6335f19291e13
Georgiy Lebedev <georgiyle@mail.ru>
2020-10-13 09:25:32 +0300

    [main.cpp]

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "log.h"
 #include "stack.h"
 
 int main()
 {
+    if (init_logging()) {
+        return EXIT_FAILURE;
+    }
+
     Stack *stack = new_stack();
 
     for (size_t i = 0; i < 100; ++i) {
         stack_push(stack, i);
     }
 
+    stack_print(stack);
+
     for (size_t i = 0; i < 90; ++i) {
         bool err;
         stack_pop(stack, &err);
     }
+
+    stack_print(stack);
 
     delete_stack(&stack);
 
